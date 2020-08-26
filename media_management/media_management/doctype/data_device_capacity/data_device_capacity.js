a759a4e1ae68e4a4428dc1d151412761108582cf
ashish-greycube <mr.ashish.shah@gmail.com>
2020-08-26 11:05:08 +0200

    feat:media all docs

---
+++ media_management/media_management/doctype/data_device_capacity/data_device_capacity.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, GreyCube Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Data Device Capacity', {
+	// refresh: function(frm) {
+
+	// }
+});
