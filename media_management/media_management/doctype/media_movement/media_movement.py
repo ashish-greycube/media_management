40c8bd6eee2740bfbac58eedb7c72fa80378b395
ashish-greycube <mr.ashish.shah@gmail.com>
2020-09-16 16:52:10 +0530

    get media items from media movement

--- media_management/media_management/doctype/media_movement/media_movement.py
+++ media_management/media_management/doctype/media_movement/media_movement.py
@@ -3,8 +3,30 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class MediaMovement(Document):
-	pass
+	def get_media(self):
+			filters={}
+			if self.customer:
+				filters.update({'customer': self.customer})
+			if self.project:
+				filters.update({'project': self.project})
+			if self.media_type:
+				filters.update({'media_type': self.media_type})
+			media_ns_list=frappe.db.get_all('Media NS',filters=filters,
+									fields=['name', 'external_id','media_owner','media_type'],
+									order_by='media_type asc,creation asc'
+							)	
+			if len(media_ns_list)>0:
+				self.media_items=[]
+				for media in media_ns_list:
+					print('media',media)
+					row = self.append('media_items', {})
+					row.media_id = media.name
+					row.external_id=media.external_id
+					row.media_owner=media.media_owner
+					row.media_type=media.media_type
+			else:
+				frappe.msgprint(frappe._('No matching Media Items found.'), indicator = 'yellow')
